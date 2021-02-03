7b10b2a1ec5bfb6efe0e3eacbea068d39289d301
guilhermeclamego <guilhermecesarl20@gmail.com>
2021-02-03 18:16:03 -0300

    Criado envio pela aplicacao

--- src/br/com/caelum/jms/TesteProdutor.java
+++ src/br/com/caelum/jms/TesteProdutor.java
@@ -5,7 +5,7 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import java.util.Scanner;
 
-public class TesteConsumidor {
+public class TesteProdutor {
     public static void main(String[] args) throws NamingException, JMSException {
 
         InitialContext context = new InitialContext();
@@ -16,23 +16,10 @@ public class TesteConsumidor {
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
         Destination fila = (Destination) context.lookup("financeiro");
-        MessageConsumer consumer = session.createConsumer(fila);
+        MessageProducer producer = session.createProducer(fila);
 
-        consumer.setMessageListener(new MessageListener() {
-            @Override
-            public void onMessage(Message message) {
-
-                //Pegar apenas a mensagem do envio
-                TextMessage textMessage = (TextMessage) message;
-                try {
-                    System.out.println(textMessage.getText());
-                } catch (JMSException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        new Scanner(System.in).nextLine();
+        Message message = session.createTextMessage("<pedido><id>123</id></pedido>");
+        producer.send(message);
 
         session.close();
         connection.close();
