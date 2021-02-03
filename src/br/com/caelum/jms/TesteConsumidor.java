ff63f1e86e2092d70674109532cfa3f9a77c3acf
guilhermeclamego <guilhermecesarl20@gmail.com>
2021-02-03 17:40:34 -0300

    Captando apenas o texto das mensagem recebidas

--- src/br/com/caelum/jms/TesteConsumidor.java
+++ src/br/com/caelum/jms/TesteConsumidor.java
@@ -1,17 +1,41 @@
 package br.com.caelum.jms;
 
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
-import javax.jms.JMSException;
+import javax.jms.*;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import java.util.Scanner;
 
 public class TesteConsumidor {
     public static void main(String[] args) throws NamingException, JMSException {
 
         InitialContext context = new InitialContext();
-
         ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+
         Connection connection = factory.createConnection();
+        connection.start();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        Destination fila = (Destination) context.lookup("financeiro");
+        MessageConsumer consumer = session.createConsumer(fila);
+
+        consumer.setMessageListener(new MessageListener() {
+            @Override
+            public void onMessage(Message message) {
+
+                //Pegar apenas a mensagem do envio
+                TextMessage textMessage = (TextMessage) message;
+                try {
+                    System.out.println("Mensagem: " + textMessage.getText());
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        new Scanner(System.in).nextLine();
+
+        session.close();
+        connection.close();
+        context.close();
     }
 }
